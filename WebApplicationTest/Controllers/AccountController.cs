6d9de7852dbcbe9daaf4f23af7c0f9bdd0944444
Ajeet <ajeet.phibonacci@gmail.com>
2022-12-21 15:34:00 +0530

    add-login

--- WebApplicationTest/Controllers/AccountController.cs
+++ WebApplicationTest/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
 using WebApplicationTest.Models;
 
 namespace WebApplicationTest.Controllers
@@ -10,17 +11,21 @@ namespace WebApplicationTest.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signManager;   
+        private readonly SignInManager<ApplicationUser> _signManager;
         public AccountController(UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signManager)
         {
             _userManager = userManager;
-            _signManager= signManager;  
+            _signManager = signManager;
         }
         [HttpPost]
         public async Task<IActionResult> Register(RegisterModel register)
         {
             var userExits = await _userManager.FindByEmailAsync(register.UserName);
+            if (userExits == null) 
+            {
+                return NotFound();  
+            }
             ApplicationUser user = new ApplicationUser()
             {
                 UserName = register.UserName,
@@ -29,6 +34,21 @@ namespace WebApplicationTest.Controllers
             };
             var reult = await _userManager.CreateAsync(user, register.Password);
             return Ok(reult);
-          }
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginModel register)
+        {
+            var userExits = await _userManager.FindByEmailAsync(register.UserName);
+            if (userExits == null)
+            {
+                return NotFound();
+            }
+            var response = await _signManager.CheckPasswordSignInAsync(userExits, register.Password,false);
+            return Ok(response); 
+
+
+              
+        }
+
     }
 }
