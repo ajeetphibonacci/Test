76fd403962fe3b7f289ca79184ba143ab330ab3d
Ajeet <ajeet.phibonacci@gmail.com>
2022-12-21 15:49:19 +0530

    remove-error

--- WebApplicationTest/Controllers/AccountController.cs
+++ WebApplicationTest/Controllers/AccountController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Win32;
 using WebApplicationTest.Models;
 
 namespace WebApplicationTest.Controllers
